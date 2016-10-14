58676148adb26dd5179dc44671ffa5e33d838a5d
khanhpdt <tungkhanh.a2@gmail.com>
2016-10-14 14:33:05 +0700

    - A more efficient solution to problem 4.3

--- src/main/java/vn/khanhpdt/playgrounds/ctci/treesandgraphs/MinimumHeightBST.java
+++ src/main/java/vn/khanhpdt/playgrounds/ctci/treesandgraphs/MinimumHeightBST.java
@@ -1,6 +1,7 @@
 package vn.khanhpdt.playgrounds.ctci.treesandgraphs;
 
 import vn.khanhpdt.playgrounds.datastructures.nodes.BinarySearchTreeNode;
+import vn.khanhpdt.playgrounds.datastructures.nodes.BinarySearchTreeNullNode;
 import vn.khanhpdt.playgrounds.datastructures.trees.BinarySearchTree;
 
 import java.util.UUID;
@@ -12,20 +13,46 @@ import java.util.UUID;
  */
 class MinimumHeightBST {
 
-	static BinarySearchTree<UUID, Integer> buildFrom(int[] sortedIntegers) {
+	static BinarySearchTree<UUID, Integer> create(int[] sortedIntegers) {
 		BinarySearchTree<UUID, Integer> bst = new BinarySearchTree<>();
-		buildFrom(sortedIntegers, 0, sortedIntegers.length - 1, bst);
+		create(sortedIntegers, 0, sortedIntegers.length - 1, bst);
 		return bst;
 	}
 
-	private static void buildFrom(int[] sortedIntegers, int from, int to, BinarySearchTree<UUID, Integer> bst) {
+	// O(NlgN)
+	private static void create(int[] sortedIntegers, int from, int to, BinarySearchTree<UUID, Integer> bst) {
 		if (from > to) {
 			return;
 		}
 
 		int middle = from + (int) Math.ceil((to - from) / 2);
+
+		// O(lgN)
 		bst.insert(BinarySearchTreeNode.from(UUID.randomUUID(), sortedIntegers[middle]));
-		buildFrom(sortedIntegers, from, middle - 1, bst);
-		buildFrom(sortedIntegers, middle + 1, to, bst);
+
+		create(sortedIntegers, from, middle - 1, bst);
+		create(sortedIntegers, middle + 1, to, bst);
 	}
+
+	// O(N)
+	static BinarySearchTree<UUID, Integer> create2(int[] sortedIntegers) {
+		BinarySearchTreeNode<UUID, Integer> root = create2(sortedIntegers, 0, sortedIntegers.length - 1);
+		return new BinarySearchTree<>(root);
+	}
+
+	private static BinarySearchTreeNode<UUID, Integer> create2(int[] sortedIntegers, int from, int to) {
+		if (from > to) {
+			return BinarySearchTreeNullNode.getInstance();
+		}
+
+		int middle = from + (int) Math.ceil((to - from) / 2);
+
+		BinarySearchTreeNode<UUID, Integer> node = BinarySearchTreeNode.from(UUID.randomUUID(), sortedIntegers[middle]);
+
+		node.setLeft(create2(sortedIntegers, from, middle - 1));
+		node.setRight(create2(sortedIntegers, middle + 1, to));
+
+		return node;
+	}
+
 }
