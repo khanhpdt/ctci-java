4a945c4b4b01f4c3f5e3988d665c0dfe6861d963
khanh <khanhpdtvn@gmail.com>
2016-11-06 00:41:39 +0700

    -  refactor

--- src/main/java/vn/khanhpdt/playgrounds/ctci/stacksandqueues/QueueByTwoStacks.java
+++ src/main/java/vn/khanhpdt/playgrounds/ctci/stacksandqueues/QueueByTwoStacks.java
@@ -28,12 +28,12 @@ public class QueueByTwoStacks {
 	public SinglyLinkedNode<UUID, Integer> dequeue() {
 		// to avoid unnecessary shifts when we can still find the first-come node in the dequeuing stack
 		if (dequeuingStack.isEmpty()) {
-			shiftStack(enqueuingStack, dequeuingStack);
+			transferStack(enqueuingStack, dequeuingStack);
 		}
 		return dequeuingStack.pop();
 	}
 
-	private void shiftStack(Stack<SinglyLinkedNode<UUID, Integer>> stackFrom, Stack<SinglyLinkedNode<UUID, Integer>> stackTo) {
+	private void transferStack(Stack<SinglyLinkedNode<UUID, Integer>> stackFrom, Stack<SinglyLinkedNode<UUID, Integer>> stackTo) {
 		SinglyLinkedNode<UUID, Integer> current = stackFrom.pop();
 		while (current != null) {
 			stackTo.push(current);
